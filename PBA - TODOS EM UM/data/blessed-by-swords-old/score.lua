dd5d68fef9bffadf8aafd7913b9da29907d757be
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-04-20 00:31:00 -0300

    feat: upload files via Android Dev Tools

---
+++ PBA - TODOS EM UM/data/blessed-by-swords-old/score.lua
@@ -0,0 +1,7 @@
+function onCreatePost()
+    doTweenColor('scoreTxt', 'scoreTxt', 'FF69B4', 0.01, 'linear');
+end
+
+function onUpdate(elapsed)
+doTweenColor('scoreTxt', 'scoreTxt', 'FF69B4', 0.01, 'linear');
+end
