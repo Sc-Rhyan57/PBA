c4cd29cddf67c97327edcf6ef99219380a17c098
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-04-20 00:31:29 -0300

    feat: upload files via Android Dev Tools

---
+++ PBA - TODOS EM UM/data/Mindless-recreate/iconMiddle.lua
@@ -0,0 +1,8 @@
+function onUpdatePost(elapsed)
+if not songName == 'Childs Play V2' then
+setProperty('iconP1.x', 620)
+setProperty('iconP2.x', 520)
+setProperty('healthBar.visible', false);
+setProperty('healthBarBG.visible', false);
+    end
+end
