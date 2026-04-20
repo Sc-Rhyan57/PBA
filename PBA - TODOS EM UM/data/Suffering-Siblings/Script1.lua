97787f821a95c44efff3735b1f1cd5199d863340
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-04-20 00:32:40 -0300

    feat: upload files via Android Dev Tools

---
+++ PBA - TODOS EM UM/data/Suffering-Siblings/Script1.lua
@@ -0,0 +1,37 @@
+x = 1050
+
+function onCreate()
+    makeLuaSprite('CN', 'cnlogo', x+600, 580)
+    scaleObject('CN', 0.43, 0.43);
+    setObjectCamera("CN", 'other')
+    addLuaSprite('CN', true)
+    if not botPlay then
+        setProperty('canPause',false)
+	end
+end
+
+function onSongStart()
+    doTweenX('CN','CN',x,1,'backOut')
+end
+
+function onTweenCompleted(t)
+    if t == 'CN' then
+        doTweenAlpha('NC','CN',0.4,1)
+    end
+end
+
+function onGameOver()
+    setProperty('CN.scale.y',0)
+    doTweenY('mariomadness','CN',0.43)
+    makeLuaSprite('CN', 'Res', x, 580)
+    scaleObject('CN', 0.43, 0.43);
+    setObjectCamera("CN", 'other')
+    addLuaSprite('CN', true)
+end
+function opponentNoteHit(noteType)
+       health = getProperty('health')
+    if getProperty('health') > 0.40 then
+       setProperty('health', health- 0.02);
+    end
+    
+end
