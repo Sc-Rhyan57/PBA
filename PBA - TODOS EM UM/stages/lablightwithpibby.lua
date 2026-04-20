e81aa8b6085ed07bedff58144921ba5379aa55ab
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-04-20 00:41:54 -0300

    feat: upload files via Android Dev Tools

---
+++ PBA - TODOS EM UM/stages/lablightwithpibby.lua
@@ -0,0 +1,5 @@
+function onCreate()
+    makeLuaSprite('z' , 'lab_light' , -800 , -700);
+    addLuaSprite('z' , false);--     1 valor  2 
+--:v donde puse awita puedes poner tu esenario y del z cualquier nombre pero tiene ser iguales como yo le puse caso contrario no funcionara :'('
+end
