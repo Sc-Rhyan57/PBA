76d89fbba23107b27c09349e08e3323db03436d2
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-04-20 00:41:53 -0300

    feat: upload files via Android Dev Tools

---
+++ PBA - TODOS EM UM/stages/lablight.lua
@@ -0,0 +1,11 @@
+function onCreate()
+    makeLuaSprite('z' , 'lab_light' , -800 , -700);
+    addLuaSprite('z' , false);--     1 valor  2 
+
+makeAnimatedLuaSprite('pibby', 'characters/pibby', 2600, 220); 
+addAnimationByPrefix('pibby', 'pibby', 'Idle', 12, true);
+scaleObject('pibby', 0.9, 0.9);
+addLuaSprite('pibby', false);
+
+--:v donde puse awita puedes poner tu esenario y del z cualquier nombre pero tiene ser iguales como yo le puse caso contrario no funcionara :'('
+end
