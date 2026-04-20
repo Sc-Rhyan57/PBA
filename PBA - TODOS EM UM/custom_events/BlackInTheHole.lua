672ae30dea399f9f17d96bf3aceb9e71b6ab7b45
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-04-20 00:37:26 -0300

    feat: upload files via Android Dev Tools

---
+++ PBA - TODOS EM UM/custom_events/BlackInTheHole.lua
@@ -0,0 +1,31 @@
+function onEvent(name,value1,value2)
+if name == 'BlackInTheHole' then
+if value1 == 'on' then
+
+doTweenColor('boyfr8272iend', 'boyfriend', '000000', 0.7, 'linear');
+doTweenColor('boy72iend', 'Jake', '000000', 0.7, 'linear');
+doTweenColor('g7272f', 'gf', '000000', 0.7, 'linear');
+doTweenColor('dadooooo', 'dad', '000000', 0.7, 'linear');
+doTweenAlpha('Niggg','Niggg',0.5,1);
+end
+if value1 == 'off' then
+doTweenColor('boyfr8272iend', 'boyfriend', 'ffffff', 0.7, 'linear');
+doTweenColor('boy2iend', 'Jake', 'ffffff', 0.7, 'linear');
+doTweenColor('g7272f', 'gf', 'ffffff', 0.7, 'linear');
+doTweenColor('dadooooo', 'dad', 'ffffff', 0.7, 'linear');
+end
+if value1 == 'on i' then
+doTweenColor('boyfr8272iend', 'boyfriend', '000000', 0.001, 'linear');
+doTweenColor('bo272iend', 'Jake', '000000', 0.001, 'linear');
+doTweenColor('g7272f', 'gf', '000000', 0.001, 'linear');
+doTweenColor('dadooooo', 'dad', '000000', 0.001, 'linear');
+end
+if value1 == 'off i' then
+doTweenColor('boyfr8272iend', 'boyfriend', 'ffffff', 0.001, 'linear');
+doTweenColor('boiend', 'Jake', 'ffffff', 0.001, 'linear');
+doTweenColor('g7272f', 'gf', 'ffffff', 0.001, 'linear');
+doTweenColor('dadooooo', 'dad', 'ffffff', 0.001, 'linear');
+doTweenAlpha('Niggg','Niggg',0,0.001);
+end
+end
+end
