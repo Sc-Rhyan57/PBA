26cb4e83dcb9e50fdd96410d2574d78668394e4e
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-04-20 00:43:30 -0300

    feat: upload files via Android Dev Tools

---
+++ PBA - TODOS EM UM/custom_notetypes/jake note.lua
@@ -0,0 +1,14 @@
+function onCreate()
+	--Iterate over all notes
+	for i = 0, getProperty('unspawnNotes.length')-1 do
+		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'jake note' then --Check if the note on the chart is a Bullet Note
+			--setPropertyFromGroup('unspawnNotes', i, 'texture', 'notetexture'); --Change notetexture
+			--setPropertyFromGroup('unspawnNotes', i, 'noteSplashHue', 0);
+			--setPropertyFromGroup('unspawnNotes', i, 'noteSplashSat', -20);
+			--setPropertyFromGroup('unspawnNotes', i, 'noteSplashBrt', 1);
+
+			setPropertyFromGroup('unspawnNotes', i, 'noAnimation', true); --Stop the animation from the original character
+			setPropertyFromGroup('unspawnNotes', i, 'noMissAnimation', true); --Dont let the original character do miss animations
+		end
+	end
+end
