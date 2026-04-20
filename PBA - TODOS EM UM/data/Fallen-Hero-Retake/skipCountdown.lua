e3cab0a3bcaced15d827c5c4458aed00d631b26d
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-04-20 00:33:21 -0300

    feat: upload files via Android Dev Tools

---
+++ PBA - TODOS EM UM/data/Fallen-Hero-Retake/skipCountdown.lua
@@ -0,0 +1,17 @@
+function onCreate()
+	
+    setProperty('showRating', false)
+	setProperty('num0', false)
+	setProperty('num1', false)
+	setProperty('num2', false)
+	setProperty('num3', false)
+	setProperty('num4', false)
+	setProperty('num5', false)
+	setProperty('num6', false)
+	setProperty('num7', false)
+	setProperty('num8', false)
+	setProperty('num9', false)
+
+end
+
+
