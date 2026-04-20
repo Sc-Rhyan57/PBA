16f11cc525de3bcfabc99a6a5e37bdd9000d24f0
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-04-20 00:42:00 -0300

    feat: upload files via Android Dev Tools

---
+++ PBA - TODOS EM UM/stages/LaboratorySsv.lua
@@ -0,0 +1,10 @@
+function onCreate()
+
+makeLuaSprite('Laboratory','Finn lab/Laboratoryno',250,-345)
+scaleObject('Laboratory',3.6,3.6)
+addLuaSprite('Laboratory',false)
+
+makeLuaSprite('Light','Finn lab/Light',250,-275)
+scaleObject('Light',3.6,3.6)
+addLuaSprite('Light',true)
+end
