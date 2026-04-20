1879f0444df0fc752e9e065733b25a6afd78421f
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-04-20 00:32:35 -0300

    feat: upload files via Android Dev Tools

---
+++ PBA - TODOS EM UM/data/Suffering-Siblings/Cache.lua
@@ -0,0 +1,65 @@
+function onCreate()
+precacheImage('characters/Jake')
+precacheImage('characters/JakeNoRTX')
+precacheImage('characters/bfdeathfinn')
+
+addCharacterToList('finn-sword', 'dad')
+addCharacterToList('finn-swordPart', 'dad')
+addCharacterToList('finn-slash', 'dad')
+addCharacterToList('finn-hurt', 'dad')
+addCharacterToList('finnNoRTX', 'dad')
+addCharacterToList('finn-hurt', 'dad')
+
+addCharacterToList('newbf cam', 'boyfriend')
+addCharacterToList('newbf camPart', 'boyfriend')
+addCharacterToList('bfNoRTX', 'boyfriend')
+addCharacterToList('bfsword', 'boyfriend')
+
+addCharacterToList('pibby', 'gf')
+addCharacterToList('pibby-angy', 'gf')
+addCharacterToList('pibby-angyPart', 'gf')
+addCharacterToList('pibby-angyNoRTX', 'gf')
+
+precacheImage('noteSplashes')
+precacheImage('NOTE_assets')
+end
+
+
+function preCacheShit()
+for i = 1,#assets do
+precacheImage(assets[i])
+end
+precacheSound(assets[i])
+precacheImage('characters');
+precacheImage('images');
+precacheSound('songs')
+precacheSound('songs/suffering-siblings/Inst')
+precacheImage('characters');
+precacheImage('hud');
+precacheImage('assets');
+precacheImage('shared');
+
+precacheImage('characters/Jake')
+precacheImage('characters/bfdeathfinn')
+precacheImage('characters/JakeNoRTX')
+
+addCharacterToList('finn-sword', 'dad')
+addCharacterToList('finn-swordPart', 'dad')
+addCharacterToList('finn-slash', 'dad')
+addCharacterToList('finn-hurt', 'dad')
+addCharacterToList('finnNoRTX', 'dad')
+addCharacterToList('finn-hurt', 'dad')
+
+addCharacterToList('newbf cam', 'boyfriend')
+addCharacterToList('newbf camPart', 'boyfriend')
+addCharacterToList('bfNoRTX', 'boyfriend')
+addCharacterToList('bfsword', 'boyfriend')
+
+addCharacterToList('pibby', 'gf')
+addCharacterToList('pibby-angy', 'gf')
+addCharacterToList('pibby-angyPart', 'gf')
+addCharacterToList('pibby-angyNoRTX', 'gf')
+
+precacheImage('noteSplashes')
+precacheImage('NOTE_assets')
+end
