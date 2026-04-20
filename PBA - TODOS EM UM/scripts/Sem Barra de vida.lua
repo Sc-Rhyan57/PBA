f0a7da4b90ddb394507df9265ee1e509c69dfe6b
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-04-20 00:39:09 -0300

    feat: upload files via Android Dev Tools

---
+++ PBA - TODOS EM UM/scripts/Sem Barra de vida.lua
@@ -0,0 +1,10 @@
+--RHYANMODS PASSOU POR AQUI E MODIFICOU ESSE SCRIPT 🤫
+
+function onUpdatePost(elapsed)
+
+setProperty('iconP1.x', 620)
+setProperty('iconP2.x', 520)
+setProperty('healthBar.visible', false);
+setProperty('healthBarBG.visible', false);
+
+end
