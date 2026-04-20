76361121c07e9919264a4ab9f2a3281a20462727
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-04-20 00:33:45 -0300

    feat: upload files via Android Dev Tools

---
+++ PBA - TODOS EM UM/data/Fallen-Hero-Retake/credits/credits intro 2.lua
@@ -0,0 +1,21 @@
+function onCreatePost()
+setTextString('botplayTxt', '0');
+end
+function onStepHit()
+	if curStep == 180 then
+
+		makeLuaText('Text', "PA [ Shikomori ]", 1800, 450, 900); 
+      setTextFont('Text', 'Thunderman.ttf')
+		setTextSize('Text', 60);
+		setObjectCamera('Text', 'other');
+		addLuaText('Text', true);
+
+		doTweenY("box", "box", -0, 0.8, "linear")	
+		doTweenY("Text", "Text", 300, 1, "linear")
+    end
+	if curStep == 9999 then
+		doTweenY("box", "box", -780, 0.8, "linear")	
+		doTweenY("Text", "Text", -350, 0.7, "linear")
+            end
+	end
+end
